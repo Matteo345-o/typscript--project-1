5069e5f2d222654d9103a5d5be9eaab76a653b5b
Matteo Primavesi <matteo.primavesi@itsincom.it>
2025-11-07 09:45:39 +0100

    implementato algoritmi calcolatrice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-/* eslint-disable no-console */
 
-function main(): void
+let isValid = true;
+let media = 0;
+let numero = 0;
+let indice = 0;
+let somma = 0;
+
+while ((isValid) && (indice < 7))
 {
-    console.log("Hello, world!");
-}
+    numero = Number(prompt("Inserisci il voto."));
+    if ((numero < 1) || (numero > 10))
+    {
+        isValid = false;
 
-main();
+        alert("Il numero inserito non è valido.");
+    }
+    else
+    {
+        numero += 1;
+        somma += numero;
+    }
+
+    indice += 1;
+}
+media = somma / numero;
+{
+    alert("La media dei voti è: " + (media));
+}
